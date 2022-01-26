1c4e1396c9962e4c5b8e0c763af10459258fd4a4
hatanoyoshihiko <66893746+hatanoyoshihiko@users.noreply.github.com>
2022-01-26 17:51:30 +0900

    add deploy wordpress for keepalived

---
+++ wordpress_deploy_ha/roles/keepalived/files/lbs_route_table_update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+VIP=10.0.0.30
+ROUTE_TABLE_ID=rtb-ce7366b3
+INSTANCE_ID=`curl -s 169.254.169.254/latest/meta-data/instance-id`
+
+export AWS_ACCESS_KEY_ID=
+export AWS_SECRET_ACCESS_KEY=
+export AWS_DEFAULT_REGION=
+
+aws --region us-east-1 ec2 delete-route --destination-cidr-block $VIP/32 --route-table-id $ROUTE_TABLE_ID
+aws --region us-east-1 ec2 create-route --destination-cidr-block $VIP/32 --route-table-id $ROUTE_TABLE_ID --instance-id $INSTANCE_ID
